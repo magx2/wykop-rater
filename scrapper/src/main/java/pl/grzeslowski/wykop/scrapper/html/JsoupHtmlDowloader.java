f4ad8bec7ae4967598b683b3b8948954fda25f9d
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-12-17 12:29:29 +0100

    if SocketTimeoutException occurs waits 60 secs

--- scrapper/src/main/java/pl/grzeslowski/wykop/scrapper/html/JsoupHtmlDowloader.java
+++ scrapper/src/main/java/pl/grzeslowski/wykop/scrapper/html/JsoupHtmlDowloader.java
@@ -4,10 +4,13 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import pl.grzeslowski.wykop.scrapper.pages.Link;
 
 import java.io.IOException;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.TimeUnit;
 
 import static java.lang.String.format;
 
@@ -15,12 +18,23 @@ import static java.lang.String.format;
 class JsoupHtmlDowloader implements HtmlDownloader{
     private static final Logger log = LoggerFactory.getLogger(JsoupHtmlDowloader.class);
 
+    @Value("${jsoup.waitAfterSocketTimeoutException}")
+    private int waitAfterSocketTimeoutException;
+
     @Override
     public Html download(Link link) {
         try {
-            log.debug("Downloading HTML for {}.", link);
+            log.info("Downloading HTML for {}", link.getLink());
             final Document document = Jsoup.connect(link.getLink()).get();
             return new Html(document);
+        } catch (SocketTimeoutException e) {
+            log.warn("Got SocketTimeoutException({}), will wait for {} secs", e.getMessage(), waitAfterSocketTimeoutException);
+            try {
+                TimeUnit.SECONDS.sleep(waitAfterSocketTimeoutException);
+            } catch (InterruptedException e1) {
+                // ignore
+            }
+            return download(link);
         } catch (IOException e) {
             throw new RuntimeException(format("Couldn't download HTML from %s", link), e);
         }
