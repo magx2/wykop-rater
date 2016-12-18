90a3e7143333d47ff62468369a9f25947a266832
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-12-19 00:44:47 +0100

    excluding JsonCrud

--- scrapper/src/test/java/pl/grzeslowski/wykop/scrapper/ScrapperApplicationTestContext.java
+++ scrapper/src/test/java/pl/grzeslowski/wykop/scrapper/ScrapperApplicationTestContext.java
@@ -5,10 +5,20 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.core.type.filter.AssignableTypeFilter;
 
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = CommandLineRunner.class))
+@ComponentScan(excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = CommandLineRunner.class),
+        @ComponentScan.Filter(type = FilterType.CUSTOM, value = ScrapperApplicationTestContext.JsonCrudAssignableTypeFilter.class)
+})
 @EnableAutoConfiguration
 public class ScrapperApplicationTestContext {
+    static class JsonCrudAssignableTypeFilter extends AssignableTypeFilter  {
+        private  JsonCrudAssignableTypeFilter() throws ClassNotFoundException {
+            super(Class.forName("pl.grzeslowski.wykop.scrapper.io.JsonCrud"));
+        }
 
+    }
 }
+
