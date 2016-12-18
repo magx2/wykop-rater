f2f3c9d9d5a58874ef27946b705e3bcdefc1411d
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-12-19 00:51:49 +0100

    more logging

--- scrapper/src/main/java/pl/grzeslowski/wykop/scrapper/pages/WykopPageIterator.java
+++ scrapper/src/main/java/pl/grzeslowski/wykop/scrapper/pages/WykopPageIterator.java
@@ -1,5 +1,7 @@
 package pl.grzeslowski.wykop.scrapper.pages;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.regex.Matcher;
@@ -9,6 +11,7 @@ import static java.lang.String.format;
 
 @Service
 class WykopPageIterator implements PageIterator {
+    private static final Logger log = LoggerFactory.getLogger(WykopPageIterator.class);
     private static final Pattern MAIN_PAGE_PATTERN = Pattern.compile("(?:http://)?www\\.wykop\\.pl(?:/)?");
     private static final Pattern PAGE_PATTERN = Pattern.compile("(?:http://)?www\\.wykop\\.pl/strona/([0-9]+)(?:/)?");
 
@@ -16,12 +19,15 @@ class WykopPageIterator implements PageIterator {
     public Link next(Link link) {
         final String linkVal = link.getLink();
         if (isMainPage(linkVal)) {
+            log.info("Going to page nr 2");
             return createNewPage(2);
         } else {
             final Matcher matcher = PAGE_PATTERN.matcher(linkVal);
             if (matcher.matches()) {
                 final int pageNumber = Integer.parseInt(matcher.group(1));
-                return createNewPage(pageNumber + 1);
+                final int nextPageNumber1 = pageNumber + 1;
+                log.info("Going to page nr {}", nextPageNumber1);
+                return createNewPage(nextPageNumber1);
             } else {
                 throw new IllegalArgumentException(format("Cannot parse this linkVal: %s!", linkVal));
             }
