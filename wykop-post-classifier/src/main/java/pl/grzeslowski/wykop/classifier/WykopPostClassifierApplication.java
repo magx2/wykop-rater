79a214309d12eed3c552c09303f365df9ae48c92
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-12-19 09:03:44 +0100

    wykop-post-classifier

---
+++ wykop-post-classifier/src/main/java/pl/grzeslowski/wykop/classifier/WykopPostClassifierApplication.java
@@ -0,0 +1,12 @@
+package pl.grzeslowski.wykop.classifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WykopPostClassifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WykopPostClassifierApplication.class, args);
+	}
+}
