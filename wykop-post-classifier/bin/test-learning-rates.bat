4f999609fef03b9d28730149db7530be8aef1966
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-12-20 17:09:27 +0100

    adding 0 as I anted before

--- wykop-post-classifier/bin/test-learning-rates.bat
+++ wykop-post-classifier/bin/test-learning-rates.bat
@@ -1,8 +1,8 @@
 java -jar build/libs/wykop-post-classifier-0.0.1-SNAPSHOT.jar --spring.profiles.active=small --rnn.learningRate=10
 java -jar build/libs/wykop-post-classifier-0.0.1-SNAPSHOT.jar --spring.profiles.active=small --rnn.learningRate=1
 java -jar build/libs/wykop-post-classifier-0.0.1-SNAPSHOT.jar --spring.profiles.active=small --rnn.learningRate=0.1
-java -jar build/libs/wykop-post-classifier-0.0.1-SNAPSHOT.jar --spring.profiles.active=small --rnn.learningRate=0.6
-java -jar build/libs/wykop-post-classifier-0.0.1-SNAPSHOT.jar --spring.profiles.active=small --rnn.learningRate=0.5
-java -jar build/libs/wykop-post-classifier-0.0.1-SNAPSHOT.jar --spring.profiles.active=small --rnn.learningRate=0.4
+java -jar build/libs/wykop-post-classifier-0.0.1-SNAPSHOT.jar --spring.profiles.active=small --rnn.learningRate=0.06
+java -jar build/libs/wykop-post-classifier-0.0.1-SNAPSHOT.jar --spring.profiles.active=small --rnn.learningRate=0.05
+java -jar build/libs/wykop-post-classifier-0.0.1-SNAPSHOT.jar --spring.profiles.active=small --rnn.learningRate=0.04
 java -jar build/libs/wykop-post-classifier-0.0.1-SNAPSHOT.jar --spring.profiles.active=small --rnn.learningRate=0.01
 java -jar build/libs/wykop-post-classifier-0.0.1-SNAPSHOT.jar --spring.profiles.active=small --rnn.learningRate=0.001
