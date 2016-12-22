3d3c517ff08f3345f0d9095662dc2e78b3188039
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2016-12-22 16:25:22 +0100

    adding profile ui

--- wykop-post-classifier/src/main/java/pl/grzeslowski/wykop/classifier/test/IterationListenerConfiguration.java
+++ wykop-post-classifier/src/main/java/pl/grzeslowski/wykop/classifier/test/IterationListenerConfiguration.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import javax.annotation.PreDestroy;
 import java.io.File;
@@ -34,6 +35,7 @@ class IterationListenerConfiguration {
     }
 
     @Bean
+    @Profile("uiserver")
     StatsListener statsListener() {
         UIServer uiServer = UIServer.getInstance();
         if(fileStatsStorage.delete()) {
